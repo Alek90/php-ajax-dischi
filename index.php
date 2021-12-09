b595bf8a4799db9a545e8e1bcaa7b05678e95604
Alek90 <4lek.90@gmail.com>
2021-12-09 15:24:12 +0100

    Milestone-1 without css.

--- index.php
+++ index.php
@@ -72,3 +72,42 @@ $response = [
         "year"=> "1987"
     ]
 ];
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php foreach ($response as $disc) : ?> 
+
+        
+        <div class="disc">
+
+            <img src= <?php echo $disc[poster] ?> alt= <?php $disc[title]. ' cover' ?>>
+
+            <h5><?php echo $disc[title] ?></h5>
+
+            <div class="info">
+
+                <div class="author"><?php echo $disc[author] ?></div>
+                <div class="year"><?php echo $disc[year] ?></div>
+                
+            </div>
+
+        </div>
+
+
+    <?php endforeach; ?>
+
+    
+</body>
+</html>
