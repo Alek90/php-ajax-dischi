9b1f70677841e6cd3e54a20bd2b0c9f6a6db6449
Alek90 <4lek.90@gmail.com>
2021-12-09 17:41:59 +0100

    Problemi con Vue.

--- milestone-2/index.php
+++ milestone-2/index.php
@@ -1,82 +1,3 @@
-<?php
-
-$response = [
-    [
-        "poster"=> "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
-        "title"=> "New Jersey",
-        "author"=> "Bon Jovi",
-        "genre"=> "Rock",
-        "year"=> "1988"
-    ],
-    [
-        "poster"=> "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
-        "title"=> "Live at Wembley 86",
-        "author"=> "Queen",
-        "genre"=> "Pop",
-        "year"=> "1992"
-    ],
-    [
-        "poster"=> "https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg",
-        "title"=> "Ten's Summoner's Tales",
-        "author"=> "Sting",
-        "genre"=> "Pop",
-        "year"=> "1993"
-    ],
-    [
-        "poster"=> "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
-        "title"=> "Steve Gadd Band",
-        "author"=> "Steve Gadd Band",
-        "genre"=> "Jazz",
-        "year"=> "2018"
-    ],
-    [
-        "poster"=> "https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg",
-        "title"=> "Brave new World",
-        "author"=> "Iron Maiden",
-        "genre"=> "Metal",
-        "year"=> "2000"
-    ],
-    [
-        "poster"=> "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
-        "title"=> "One more car, one more raider",
-        "author"=> "Eric Clapton",
-        "genre"=> "Rock",
-        "year"=> "2002"
-    ],
-    [
-        "poster"=> "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
-        "title"=> "Made in Japan",
-        "author"=> "Deep Purple",
-        "genre"=> "Rock",
-        "year"=> "1972"
-    ],
-    [
-        "poster"=> "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
-        "title"=> "And Justice for All",
-        "author"=> "Metallica",
-        "genre"=> "Metal",
-        "year"=> "1988"
-    ],
-    [
-        "poster"=> "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
-        "title"=> "Hard Wired",
-        "author"=> "Dave Weckl",
-        "genre"=> "Jazz",
-        "year"=> "1994"
-    ],
-    [
-        "poster"=> "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
-        "title"=> "Bad",
-        "author"=> "Michael Jacjson",
-        "genre"=> "Pop",
-        "year"=> "1987"
-    ]
-];
-
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -102,29 +23,25 @@ $response = [
 
         <div class="row mx-4 gy-4 gx-5 justify-content-center">
 
-            
-            <?php foreach ($response as $disc) : ?> 
+
+            <div class="col-2" v-for= "disc in discs">
+
+                <div class="disc p-4 text-center">
                 
-                <div class="col-2">
-                    
-                    <div class="disc p-4 text-center">
-            
-                        <img src= <?php echo $disc[poster] ?> alt= <?php $disc[title]. ' cover' ?>>
-            
-                        <h5 class="text-white my-4"><?php echo $disc[title] ?></h5>
-            
-                        <div class="info">
-            
-                            <div class="author"><?php echo $disc[author] ?></div>
-                            <div class="year"><?php echo $disc[year] ?></div>
-                            
-                        </div>
-            
+                    <img :src="disc.poster"  alt= '' >
+
+                    <h5 class="text-white my-4"> {{disc.title}} </h5>
+
+                    <div class="info">
+
+                        <div class="author"> {{disc.author}} </div>
+                        <div class="year"> {{disc.year}} </div>
+                        
                     </div>
-            
+
                 </div>
-            
-            <?php endforeach; ?>
+
+            </div>
 
 
         </div>
@@ -139,7 +56,7 @@ $response = [
     <!-- script axios -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.24.0/axios.min.js" integrity="sha512-u9akINsQsAkG9xjc1cnGF4zw5TFDwkxuc9vUp5dltDWYCSmyd0meygbvgXrlc/z7/o4a19Fb5V0OUE58J7dcyw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- script vue js -->
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <!-- script file js -->
     <script src="./assets/app.js"></script>
     
